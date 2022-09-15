958b9a0dace0c335e46c098dc16c22f0a7152d1b
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2022-09-15 23:08:00 +0100

    fix rebase

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/error', [Controller::class, 'error'])->name('error route');
 Route::get('webhook', [GithubWebhookController::class, 'index']);
 Route::post('webhook', [GithubWebhookController::class, 'store']);
 
-
 Route::post('register/organization', [OrganizationController::class, 'store'])->name('register.store')->middleware('verify.services');
 Route::get('organization/{organization}', [OrganizationController::class, 'show']);
+Route::post('repositorydetails', [OrganizationRepositoryDetailsController::class, 'getOrganizationMembers'])->name('repositorydetails');
 Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
