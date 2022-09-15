81a8ea30b8460cd681a7a0763aa72fc8971ea1b3
Chisom Ejim <ejimchisom@gmail.com>
2022-09-15 14:49:28 +0100

    WIP

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,25 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\WebhookLog;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 
-class GithubWebhookController extends Controller
+class WebhookController extends Controller
 {
 
     public function index() {
-        $payload = WebhookLog::query()->find(1)->data->toArray();
+        $payload = WebhookLog::query()->find(7)->data->toArray();
         return json_decode(json_encode($payload), true);
     }
 
-    public function store(Request $request): Response|JsonResponse
+    public function store(Request $request): JsonResponse
     {
         // pass it to job
         // save the webhook to database with pending default status
         // process the webhook
         // update the status success
 
+        logger($request);
         $data = json_decode($request['payload'], true);
         WebhookLog::create([
             'vendor' => 'github',
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\GithubWebhookController;
+use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,40 @@ Route::get('/', [Controller::class, 'routes'])
     ->withoutMiddleware('api');
 Route::get('/example', [Controller::class, 'example'])->name('example route');
 Route::get('/error', [Controller::class, 'error'])->name('error route');
-Route::get('webhook', [GithubWebhookController::class, 'index']);
-Route::post('webhook', [GithubWebhookController::class, 'store']);
+Route::get('webhook', [WebhookController::class, 'index']);
+Route::post('webhook', [WebhookController::class, 'store']);
+
+Route::get('testing', function () {
+    $response = Http::withHeaders(
+        [
+            'Accept' => 'application/vnd.github+json',
+            'Authorization' => 'Bearer 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'
+        ]
+    )->post('https://api.github.com/repos/acellware/onboard/hooks',
+        [
+            'name' => 'web',
+            'active' => true,
+            'events' => [
+                "discussion",
+                "pull_request",
+                "pull_request_review",
+                "pull_request_review_comment",
+                "repository",
+                "status",
+            ],
+            'config' => [
+                'url' => "https://a9f1-102-89-41-214.eu.ngrok.io/webhook",
+                'content_type' => "form",
+                'insecure_ssl' => "0",
+            ]
+        ]
+    );
+
+    if($response->failed()) {
+        // throw exception
+        return ['failed' => $response->json()];
+    }
+
+    return $response->json();
+
+});
